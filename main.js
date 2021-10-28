a840ee69c6e19426e8f7192e29a4f409a639facd
S-R-Code <83865650+S-R-Code@users.noreply.github.com>
2021-10-28 15:49:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 song = "";
+leftWristX = "";
+rightWristX = "";
+leftWristY = "";
+rightWristY = "";
+
+
 
 function setup() {
     canvas = createCanvas(600, 500);
@@ -7,16 +13,45 @@ function setup() {
     video = createCapture(VIDEO);
     video.hide();
 
+    poseNet.on('pose' , gotPoses );
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+
+}
+
+function modelLoaded() {
+ console.log("Posenet is Initailized");
 }
 
 function preload(){
-    song = loadSound("music.mp3")
+    song = loadSound("music.mp3");
 }
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
 
 function draw() {
     image(video , 0 , 0 , 600 , 500);
+}
+
+function gotPoses(results){
+
+    if(results.length > 0 ){
+        console.log(results);
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        
+        console.log( "leftWristX =  " + leftWristX  ,  "leftWristY = " + leftWristY  );
+
+        rightWristX = results[0].pose.rightWrist.x;
+
+        rightWristY = results[0].pose.rightWrist.y;
+        
+        console.log( "rightWristX =" + rightWristX ,   " + rightWristY" = rightWristY);
+    }
+
 }
